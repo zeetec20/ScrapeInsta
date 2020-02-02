535182589a1de03eae504ab0f5595b4b1fe1eab8
zeetec20 <jusles363@gmail.com>
2020-02-02 13:21:29 +0700

    add search

--- asset/script/script.js
+++ asset/script/script.js
@@ -1,7 +1,7 @@
 let posts
 let listPost = document.getElementById('allPost')
 let countGetPost = 1
-let getPostActive = true
+let getPostActive = (end_cursor == "" ? false : true)
 let offset = document.querySelector('.offset1').offsetTop
 
 window.onscroll = function () {
@@ -11,9 +11,7 @@ window.onscroll = function () {
         axios.get(`${mainPath}/post/?id=${id_user}&end_cursor=${end_cursor}&post=get_post`)
         .then(function (response){
             response = response['data']
-            console.log(response)
             if (response['success']) {
-                console.log(JSON.parse(response['result']))
                 response = JSON.parse(response['result'])['data']['user']
                 posts = response['edge_owner_to_timeline_media']['edges']
                 end_cursor = response['edge_owner_to_timeline_media']['page_info']['end_cursor']
@@ -26,18 +24,18 @@ window.onscroll = function () {
                 countGetPost += 1
                 posts.forEach((value, key) => {
                     let post = `<div class="profile__photo ${key == 8 ? `offset${countGetPost}` : ''}">
-                                <div style="background-image: url('${value['node']['display_url']}'); background-size: <?=$width?>%; height: 290px; width: 290px; background-position:center;">&nbsp;</div>
-                                <div class="profile__photo-overlay">
-                                    <span class="overlay__item">
-                                        <i class="fa fa-heart"></i>
-                                        ${value['node']['edge_media_preview_like']['count']}
-                                    </span>
-                                    <span class="overlay__item">
-                                        <i class="fa fa-comment"></i>
-                                        ${value['node']['edge_media_to_comment']['count']}
-                                    </span>
-                                </div>
-                            </div>`
+                                    <div class="image" style="background-image:url('${value['node']['display_url']}');">&nbsp;</div>
+                                    <div class="profile__photo-overlay">
+                                        <span class="overlay__item">
+                                            <i class="fa fa-heart"></i>
+                                            ${value['node']['edge_media_preview_like']['count']}
+                                        </span>
+                                        <span class="overlay__item">
+                                            <i class="fa fa-comment"></i>
+                                            ${value['node']['edge_media_to_comment']['count']}
+                                        </span>
+                                    </div>
+                                </div>`
                     
                     listPost.innerHTML = listPost.innerHTML + post
                 })
@@ -48,7 +46,5 @@ window.onscroll = function () {
                 console.log(response)
             }
         })
-
-
     }
 }
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$username = 'hello.darwis'; 
+$username = null;
 
 function scrape_insta($username) {
 	$insta_source = file_get_contents('https://www.instagram.com/'.$username);
@@ -9,20 +9,24 @@ function scrape_insta($username) {
 	return $insta_array;
 }
 
-$results_array = scrape_insta($username)['entry_data']['ProfilePage'][0]['graphql']['user'];
+if (isset($_GET['username'])) {
+    $username = $_GET['username'];
+    
+    $results_array = scrape_insta($username)['entry_data']['ProfilePage'][0]['graphql']['user'];
 
-$linkAccount = 'http://instagram.com/'.$username;
-$id = $results_array['id'];
-$end_cursor = str_replace('==', '', $results_array['edge_owner_to_timeline_media']['page_info']['end_cursor']);
-$fullname = $results_array['full_name'];
-$listPost = $results_array['edge_owner_to_timeline_media']['edges'];
-$profile_pic = $results_array['profile_pic_url_hd'];
-$follower = $results_array['edge_followed_by']['count'];
-$following = $results_array['edge_follow']['count'];
-$countPost = $results_array['edge_owner_to_timeline_media']['count'];
-$biography = $results_array['biography'];
+    $linkAccount = 'http://instagram.com/'.$username;
+    $id = $results_array['id'];
+    $end_cursor = str_replace('==', '', $results_array['edge_owner_to_timeline_media']['page_info']['end_cursor']);
+    $fullname = $results_array['full_name'];
+    $listPost = $results_array['edge_owner_to_timeline_media']['edges'];
+    $profile_pic = $results_array['profile_pic_url_hd'];
+    $follower = $results_array['edge_followed_by']['count'];
+    $following = $results_array['edge_follow']['count'];
+    $countPost = $results_array['edge_owner_to_timeline_media']['count'];
+    $biography = $results_array['biography'];
 
-$mainPath = '/'.explode('/', dirname($_SERVER['PHP_SELF']))[1];
+    $mainPath = '/'.explode('/', dirname($_SERVER['PHP_SELF']))[1];
+} 
 ?>
 
 
@@ -37,12 +41,7 @@ $mainPath = '/'.explode('/', dirname($_SERVER['PHP_SELF']))[1];
     <script src="https://rawgit.com/jackmoore/autosize/master/dist/autosize.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="./asset/css/styles.css">
-    
-    <script>
-        let id_user = '<?=$id?>'
-        let end_cursor = '<?=$end_cursor?>'
-        let mainPath = '<?=$mainPath?>'
-    </script>
+    <link rel="stylesheet" href="./asset/css/custom.css">
 </head>
 
 <body>
@@ -52,121 +51,110 @@ $mainPath = '/'.explode('/', dirname($_SERVER['PHP_SELF']))[1];
                 <img src="images/logo.png" />
             </a>
         </div>
-        <!-- <div class="navigation__column">
-            <i class="fa fa-search"></i>
-            <input type="text" placeholder="Search">
-        </div> -->
+        <div class="navigation__column">
+            <form action="" method="get">
+            <label for="submit"><i class="fa fa-search"></i></label>
+            <input type="text" name="username" placeholder="Search" <?=($username == null ? autofocus : '')?>>
+            <button id="submit" type="submit" style="display: none;"></button>
+            </form>
+        </div>
         <div class="navigation__column">
             <ul class="navigations__links">
-                <!-- <li class="navigation__list-item">
-                    <a href="explore.html" class="navigation__link">
-                        <i class="fa fa-compass fa-lg"></i>
-                    </a>
-                </li>
                 <li class="navigation__list-item">
-                    <a href="#" class="navigation__link">
-                        <i class="fa fa-heart-o fa-lg"></i>
-                    </a>
-                </li> -->
-                <li class="navigation__list-item">
-                    <a href="<?=$linkAccount?>" class="navigation__link" target="_blank" rel="noopener">
+                    <a <?=(isset($linkAccount) ? "href=\"$linkAccount\"" : '')?> class="navigation__link" target="_blank" rel="noopener" disabled>
                         <i class="fa fa-user-o fa-lg"></i>
                     </a>
                 </li>
             </ul>
         </div>
     </nav>
-    <main id="profile">
-        <header class="profile__header">
-            <div class="profile__column">
-                <img src="<?=$profile_pic?>" width="150px" />
-            </div>
-            <div class="profile__column">
-                <div class="profile__title">
-                    <h3 class="profile__username"><?=$username?></h3>
-                    <!-- <a href="edit-profile.html">Edit profile</a> -->
-                    <!-- <i class="fa fa-cog fa-lg"></i> -->
+    <?php if ($username != null):?>
+        <main id="profile">
+            <header class="profile__header">
+                <div class="profile__column">
+                    <img src="<?=$profile_pic?>" width="150px" />
                 </div>
-                <ul class="profile__stats">
-                    <li class="profile__stat">
-                        <span class="stat__number"><?=$countPost?></span> posts
-                    </li>
-                    <li class="profile__stat">
-                        <span class="stat__number"><?=$follower?></span> followers
-                    </li>
-                    <li class="profile__stat">
-                        <span class="stat__number"><?=$following?></span> following
-                    </li>
-                </ul>
-                <p class="profile__bio">
-                    <span class="profile__full-name">
-                        <?=$fullname?>
-                        <br>
-                    </span> 
-                    <textarea id="bio" name="" readonly><?=$biography?></textarea>
-                    <script>
-                        autosize(document.getElementById('bio'))
-                    </script>
-                    <style>
-                        #bio{
-                            border: none; 
-                            outline: none; 
-                            resize: none; 
-                            background-color: white;
-                            font-family: "Open Sans", sans-serif;
-                            font-size: 14px;
-                            width: 100%;
-                        }
-                    </style>
-                </p>
-            </div>
-        </header>
-        <section class="profile__photos" id="allPost">
-            <?php
-            foreach ($listPost as $key => $value):
-            ?>
-                <div class="profile__photo <?=$key == 8 ? 'offset1' : ''?>">
-                <div style="background-image:url('<?=$value['node']['display_url']?>'); background-size: 100%; height: 290px; width: 290px; background-position:center;">&nbsp;</div>
-                    <div class="profile__photo-overlay">
-                        <span class="overlay__item">
-                            <i class="fa fa-heart"></i>
-                            <?=$value['node']['edge_liked_by']['count']?>
-                        </span>
-                        <span class="overlay__item">
-                            <i class="fa fa-comment"></i>
-                            <?=$value['node']['edge_media_to_comment']['count']?>
-                        </span>
+                <div class="profile__column">
+                    <div class="profile__title">
+                        <h3 class="profile__username"><?=$username?></h3>
+                        <!-- <a href="edit-profile.html">Edit profile</a> -->
+                        <!-- <i class="fa fa-cog fa-lg"></i> -->
                     </div>
+                    <ul class="profile__stats">
+                        <li class="profile__stat">
+                            <span class="stat__number"><?=$countPost?></span> posts
+                        </li>
+                        <li class="profile__stat">
+                            <span class="stat__number"><?=$follower?></span> followers
+                        </li>
+                        <li class="profile__stat">
+                            <span class="stat__number"><?=$following?></span> following
+                        </li>
+                    </ul>
+                    <p class="profile__bio">
+                        <span class="profile__full-name">
+                            <?=$fullname?>
+                            <br>
+                        </span> 
+                        <textarea id="bio" name="" readonly><?=$biography?></textarea>
+                    </p>
                 </div>
-            <?php
-            endforeach;
-            ?>
-        </section>
-    </main>
-    <footer class="footer">
-        <div class="footer__column">
-            <nav class="footer__nav">
-                <ul class="footer__list">
-                    <li class="footer__list-item"><a href="#" class="footer__link">About Us</a></li>
-                    <li class="footer__list-item"><a href="#" class="footer__link">Support</a></li>
-                    <li class="footer__list-item"><a href="#" class="footer__link">Blog</a></li>
-                    <li class="footer__list-item"><a href="#" class="footer__link">Press</a></li>
-                    <li class="footer__list-item"><a href="#" class="footer__link">Api</a></li>
-                    <li class="footer__list-item"><a href="#" class="footer__link">Jobs</a></li>
-                    <li class="footer__list-item"><a href="#" class="footer__link">Privacy</a></li>
-                    <li class="footer__list-item"><a href="#" class="footer__link">Terms</a></li>
-                    <li class="footer__list-item"><a href="#" class="footer__link">Directory</a></li>
-                    <li class="footer__list-item"><a href="#" class="footer__link">Language</a></li>
-                </ul>
-            </nav>
-        </div>
-        <div class="footer__column">
-            <span class="footer__copyright">© 2020 Instagram</span>
-        </div>
-    </footer>
+            </header>
+            <section class="profile__photos" id="allPost">
+                <?php
+                foreach ($listPost as $key => $value):
+                ?>
+                    <div class="profile__photo <?=$key == 8 ? 'offset1' : ''?>">
+                    <div class="image" style="background-image:url('<?=$value['node']['display_url']?>');">&nbsp;</div>
+                        <div class="profile__photo-overlay">
+                            <span class="overlay__item">
+                                <i class="fa fa-heart"></i>
+                                <?=$value['node']['edge_liked_by']['count']?>
+                            </span>
+                            <span class="overlay__item">
+                                <i class="fa fa-comment"></i>
+                                <?=$value['node']['edge_media_to_comment']['count']?>
+                            </span>
+                        </div>
+                    </div>
+                <?php
+                endforeach;
+                ?>
+            </section>
+        </main>
+        <footer class="footer">
+            <div class="footer__column">
+                <nav class="footer__nav">
+                    <ul class="footer__list">
+                        <li class="footer__list-item"><a href="#" class="footer__link">About Us</a></li>
+                        <li class="footer__list-item"><a href="#" class="footer__link">Support</a></li>
+                        <li class="footer__list-item"><a href="#" class="footer__link">Blog</a></li>
+                        <li class="footer__list-item"><a href="#" class="footer__link">Press</a></li>
+                        <li class="footer__list-item"><a href="#" class="footer__link">Api</a></li>
+                        <li class="footer__list-item"><a href="#" class="footer__link">Jobs</a></li>
+                        <li class="footer__list-item"><a href="#" class="footer__link">Privacy</a></li>
+                        <li class="footer__list-item"><a href="#" class="footer__link">Terms</a></li>
+                        <li class="footer__list-item"><a href="#" class="footer__link">Directory</a></li>
+                        <li class="footer__list-item"><a href="#" class="footer__link">Language</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="footer__column">
+                <span class="footer__copyright">© 2020 Instagram</span>
+            </div>
+        </footer>
 
-    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-    <script src="./asset/script/script.js"></script>
+        <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+        <script>
+            autosize(document.getElementById('bio'))
+            let id_user = '<?=$id?>'
+            let end_cursor = '<?=$end_cursor?>'
+            let mainPath = '<?=$mainPath?>'
+        </script>
+        <script src="./asset/script/script.js"></script>
+    <?php else:?>
+        
+    <?php endif;?>
 </body>
 
 </html>
